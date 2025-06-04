0607bfb972e7c52491de9ce2e20a3ccb00d6e75f
Craig Giddings <randomhero_345@hotmail.com>
2025-06-03 22:34:38 -0400

    Created protocol.py. Added a MessageHeaders class for the protocol specifications

---
+++ protocol.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+
+# frozen=True: Makes properties immutable (read-only)
+@dataclass(frozen=True) 
+class MessageHeaders:
+    HELLO: str = "HELLO"
+    HELLO_FROM: str = "HELLO_FROM"
+    IN_USE: str = "IN-USE"
+    BUSY: str = "BUSY"
+    LIST_OK: str = "LIST-OK"
+    SEND_OK: str = "SEND-OK"
+    BAD_DEST_USR: str = "BAD-DEST-USR"
+    DELIVERY: str = "DELIVERY"
+    BAD_RQST_HDR: str = "BAD-RQST-HDR"
+    BAD_RQST_BODY: str = "BAD-RQST-BODY"
